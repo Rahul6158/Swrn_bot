94e1137cb70b06c23e370f31625d3adec031e94f
Rahul6158 <128017087+Rahul6158@users.noreply.github.com>
2024-07-01 01:41:59 +0530

    Update code1.py

--- code1.py
+++ code1.py
@@ -1,12 +1,13 @@
 import streamlit as st
 import requests
 
-# Function to query OpenAI GPT-2 model
-def query_openai_gpt2(payload):
-    API_URL = "https://api-inference.huggingface.co/models/openai-community/gpt2"
-    headers = {"Authorization": "Bearer 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"}
+API_URL = "https://api-inference.huggingface.co/models/meta-llama/Llama-2-7b-hf"
+HEADERS = {
+    "Authorization": "Bearer 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"
+}
 
-    response = requests.post(API_URL, headers=headers, json=payload)
+def query(payload):
+    response = requests.post(API_URL, headers=HEADERS, json=payload)
     return response.json()
 
 # Streamlit configuration
@@ -32,7 +33,9 @@ with col2:
 submit = st.button("Generate")
 
 # Handle submission
-if submit:
-    prompt = f"Write a blog for {blog_style} for a topic {input_text} within {no_words} words."
-    response = query_openai_gpt2({"inputs": prompt})
-    st.write(response)  # Print the entire response object for inspection
+if submit and input_text:
+    payload = {
+        "inputs": f"Write a blog for {blog_style} for a topic {input_text} within {no_words} words."
+    }
+    response = query(payload)
+    st.write(response)
